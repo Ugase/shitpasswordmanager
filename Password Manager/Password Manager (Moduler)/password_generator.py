948a629c100ba005223a3a2ae352e6b6ff7af99e
Ugase <106616134+Ugase@users.noreply.github.com>
2023-10-20 17:48:30 +0300

    Delete Password Manager directory

--- Password Manager/Password Manager (Moduler)/password_generator.py
+++
@@ -1,8 +0,0 @@
-from secrets import choice
-from string import digits, ascii_letters, punctuation
-def generator(num_of_chr:int) -> str:
-    """Generates a random list of numbers, letters and symbols"""
-    sbol_list = digits + ascii_letters + punctuation
-    result = "".join(choice(sbol_list) for _ in range(num_of_chr))
-    result.replace("\\", "")
-    return result
